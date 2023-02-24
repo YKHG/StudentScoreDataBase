054b326d369cb939152ab3aec3bf7107840b701c
YKHG <sjxyxh123@gmail.com>
2023-02-24 16:51:49 +0800

    w

--- app/src/main/java/com/example/studentscoredatabase/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.studentscoredatabase
-
-class User (var id: Int, val subject:String , val score: String) {
-    override fun toString(): String { // return the record detail
-        return "$id: $subject($score)"
-    }
-}
